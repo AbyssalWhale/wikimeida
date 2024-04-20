60e127511e12b2f6c8181dee41972a44cb704607
AbyssalWhale <53709071+AbyssalWhale@users.noreply.github.com>
2024-04-20 17:57:11 +0200

    adding scenario 2 with models

---
+++ wikimediatests/wikimediatests/Models/API/ResponseModels/Pages/SinglePageDetails.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace wikimediatests.Models.API.ResponseModels.Pages
+{
+    public class SinglePageDetails
+    {
+        public int id { get; set; }
+        public string key { get; set; }
+        public string title { get; set; }
+        public Latest latest { get; set; }
+        public string content_model { get; set; }
+        public License license { get; set; }
+        public string html_url { get; set; }
+    }
+}
--- wikimediatests/wikimediatests/Tests/API/SearchTests.cs
+++ wikimediatests/wikimediatests/Tests/API/SearchTests.cs
@@ -4,13 +4,14 @@ using wikimediatests.Tests.Fixtures;
 using FluentAssertions;
 using System.Text.Json;
 using wikimediatests.Models.API.ResponseModels.Search.Pages;
+using wikimediatests.Models.API.ResponseModels.Pages;
 
 namespace wikimediatests.Tests.API
 {
     public class Tests : TestsCoreFixtures
     {
         private SearchRoute SearchRoute;
-        private static string DefaultQueru = "furry rabbits";
+        private static string QueryDefault = "furry rabbits";
         private SearchPageResultModel PagesFromSearchResult;
 
         [SetUp]
@@ -19,7 +20,7 @@ namespace wikimediatests.Tests.API
             SearchRoute = new SearchRoute(requestContext: PlaywrightRequest);
 
             // WHEN - A search for pages containing for ‘furry rabbits’ is executed
-            var requestResponse = await SearchRoute.GetSearchPageResult(query: DefaultQueru);
+            var requestResponse = await SearchRoute.GetSearchPageResult(query: QueryDefault);
             requestResponse.StatusText.Should().Be(HttpStatusCode.OK.ToString());
 
             var requestResponseBody = requestResponse.JsonAsync().Result;
@@ -35,6 +36,27 @@ namespace wikimediatests.Tests.API
             // THEN - A page with the title ‘Sesame Street’ is found
             var isContainingPage = PagesFromSearchResult.pages.Any(p => p.title.Equals(expectedPageToExist));
             isContainingPage.Should().BeTrue();
+        }
+
+        [Test]
+        [TestCase("Sesame Street", "2023-08-17")]
+        public async Task PageHasExpectedTimeStamp(string pageUnderTest, string expectedTimeStamp)
+        {
+            // GIVEN - The result for ‘furry rabbits’ search contains ‘Sesame Street’
+            var pageSearchDetails = PagesFromSearchResult.pages.FirstOrDefault(p => p.title.Equals(pageUnderTest));
+            pageSearchDetails.Should().NotBeNull();
+
+            // WHEN - The page details for Sesame Street are requested
+            var pageRoutes = new PagesRoute(requestContext: PlaywrightRequest);
+            var pageRequestResponse = await pageRoutes.GetPage(page: pageSearchDetails);
+            pageRequestResponse.StatusText.Should().Be(HttpStatusCode.OK.ToString());
+
+            // THEN - It has a latest timestamp > 2023-08-17
+            var requestResponseBody = pageRequestResponse.JsonAsync().Result;
+            requestResponseBody.Should().NotBeNull();
+            var pageDetails = requestResponseBody.Value.Deserialize<SinglePageDetails>();
+            DateTime.TryParse(expectedTimeStamp, out var pageExpectedTime);
+            pageDetails.latest.timestamp.Should().BeAfter(pageExpectedTime);
         }
     }
 }
