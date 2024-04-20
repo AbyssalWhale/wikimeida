ab1317a945c99a22f4442e8c0e10005fddd5e4eb
AbyssalWhale <53709071+AbyssalWhale@users.noreply.github.com>
2024-04-20 16:10:41 +0200

    folder structure and test fixture
    - adding folders structure
    - adding fixture class

---
+++ wikimediatests/wikimediatests/Tests/Fixtures/TestsCoreFixtures.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System.Collections.Concurrent;
+
+namespace wikimediatests.Tests.Fixtures
+{
+    [TestFixture]
+    public class TestsCoreFixtures
+    {
+        [OneTimeSetUp]
+        public async Task OneTimeSetup()
+        {
+            await Console.Out.WriteLineAsync("One time Set Up was executed");
+        }
+    }
+}
