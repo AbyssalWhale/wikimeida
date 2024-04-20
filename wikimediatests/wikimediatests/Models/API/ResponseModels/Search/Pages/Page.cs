ddf7ce00f1fa18191886e4b8b53855942a496072
AbyssalWhale <53709071+AbyssalWhale@users.noreply.github.com>
2024-04-20 17:18:09 +0200

    adding PageWithTitleCanBeFound

---
+++ wikimediatests/wikimediatests/Models/API/ResponseModels/Search/Pages/Page.cs
@@ -0,0 +1,13 @@
+﻿namespace wikimediatests.Models.API.ResponseModels.Search.Pages
+{
+    public class Page
+    {
+        public int id { get; set; }
+        public string key { get; set; }
+        public string title { get; set; }
+        public string excerpt { get; set; }
+        public object matched_title { get; set; }
+        public string description { get; set; }
+        public Thumbnail thumbnail { get; set; }
+    }
+}
---
+++ wikimediatests/wikimediatests/Models/API/ResponseModels/Search/Pages/Thumbnail.cs
@@ -0,0 +1,11 @@
+﻿namespace wikimediatests.Models.API.ResponseModels.Search.Pages
+{
+    public class Thumbnail
+    {
+        public string mimetype { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+        public object duration { get; set; }
+        public string url { get; set; }
+    }
+}
